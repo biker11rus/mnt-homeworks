950be57df843d8d7fa06af1771965b516ff4cd16
rkhozyainov <biker11rus@gmail.com>
2022-02-14 01:04:26 +0300

    task 08-03

---
+++ 08-ansible-03-yandex/playbook/yc/variables.tf
@@ -0,0 +1,15 @@
+variable "yandex_cloud_id" {
+  default = "b1g99cj6gj1uugbm8csl"
+}
+variable "yandex_folder_id" {
+  default = "b1g21i3gads8jqf0fgei"
+}
+variable "iso_id" {
+  default = "fd8gdnd09d0iqdu7ll2a"
+}
+variable "yandex_zone" {
+  default = "ru-central1-a"
+}
+variable "user" {
+  default = "centos"
+}
